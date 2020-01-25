af8f0f1b10d76b9a5dc977e4d7aeb5f177e73be7
Luka Radovanovic <lukadlm97@gmail.com>
2020-01-25 21:18:20 +0100

    Created model and controller

---
+++ DrinkAndGo/Controllers/DrinkController.cs
@@ -0,0 +1,27 @@
+﻿using DrinkAndGo.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkAndGo.Controllers
+{
+    public class DrinkController:Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IDrinkRepository _drinkRepository;
+
+        public DrinkController(ICategoryRepository categoryRepository,IDrinkRepository drinkRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _drinkRepository = drinkRepository;
+        }
+
+        public ViewResult List()
+        {
+            var drinks = _drinkRepository.Drinks;
+            return View(drinks);
+        }
+    }
+}
---
+++ DrinkAndGo/Data/Mocks/MockCategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using DrinkAndGo.Data.Interfaces;
+using DrinkAndGo.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkAndGo.Data.Mocks
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> Categories
+        {
+            get
+            {
+                return new List<Category>
+                {
+                    new Category
+                    {
+                        CategoryName="Alcoholic",Description="All alcoholic drunks"
+                    },
+                    new Category
+                    {
+                        CategoryName="Non-alcoholic",Description="Non-all alcoholic drunks"
+                    }
+                };
+            }
+        }
+    }
+}
